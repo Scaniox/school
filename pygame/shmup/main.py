72108309f31877df43ed63518e4e042afb575e63
Scaniox <alexl.mills3@gmail.com>
2020-10-22 09:59:29 +0100

    removed tests

--- pygame/shmup/main.py
+++ pygame/shmup/main.py
@@ -1,7 +1,4 @@
 # pygame shmup game
-
-# i have made some edits here
-# and some more edits there
 #modules---------------------------------------------------------------------------
 import pygame, random
 
